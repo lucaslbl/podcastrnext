564524771ac9b30e95668f4aeaecf0ef1549ade0
Lucas Lima <lucaslbl@outlook.com>
2021-04-25 23:26:22 -0300

    added tags and favicon

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -9,6 +9,7 @@ import Link from "next/link";
 
 import styles from "./episode.module.scss";
 import { usePlayer } from "../../contexts/PlayerContext";
+import Head from "next/head";
 
 type Episode = {
   id: string;
@@ -30,6 +31,10 @@ export default function Episode({ episode }: EpisodeProps) {
 
   return (
     <div className={styles.episode}>
+      <Head>
+        <title>{episode.title}| Podcastr </title>
+      </Head>
+
       <div className={styles.thumbnailContainer}>
         <Link href="/">
           <button>
